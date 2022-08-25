c7e954bafd14587c0f1cf70b02ed74ac82d1160a
Chris <chris.frazier@searchspring.com>
2022-08-25 14:10:44 -0700

    adding landing page title to search header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -8,41 +8,47 @@ export const SearchHeader = observer((props) => {
 		store: { pagination, search },
 	} = controller;
 	const originalQuery = search.originalQuery;
-
+	const landingPage = controller.store.landingPage;
 	return (
 		store.loaded && (
 			<div class="ss__search-header">
-				{pagination.totalResults ? (
-					<h1 class="ss__search-header--results">
-						{`Showing `}
-						{pagination.multiplePages && <span class="ss__search-header__count-range">{` ${pagination.begin} - ${pagination.end} of `}</span>}
-						<span class="ss__search-header__count-total">{pagination.totalResults}</span>
-						{` result${pagination.totalResults == 1 ? '' : 's'}`}
-						{search?.query && (
-							<span>
-								{` for `}
-								<span class="ss__search-header__query">"{search.query.string}"</span>
-							</span>
-						)}
-					</h1>
+				{landingPage ? (
+					<h3 className="ss__search-header--landingPageTitle">{landingPage.title}</h3>
 				) : (
-					pagination.totalResults === 0 && (
-						<h1 class="ss__search-header--noresults">
-							{search?.query ? (
-								<span>
-									No results for <span class="ss__search-header__query">"{search.query.string}"</span> found.
-								</span>
-							) : (
-								<span>No results found.</span>
-							)}
-						</h1>
-					)
-				)}
+					<>
+						{pagination.totalResults ? (
+							<h1 class="ss__search-header--results">
+								{`Showing `}
+								{pagination.multiplePages && <span class="ss__search-header__count-range">{` ${pagination.begin} - ${pagination.end} of `}</span>}
+								<span class="ss__search-header__count-total">{pagination.totalResults}</span>
+								{` result${pagination.totalResults == 1 ? '' : 's'}`}
+								{search?.query && (
+									<span>
+										{` for `}
+										<span class="ss__search-header__query">"{search.query.string}"</span>
+									</span>
+								)}
+							</h1>
+						) : (
+							pagination.totalResults === 0 && (
+								<h1 class="ss__search-header--noresults">
+									{search?.query ? (
+										<span>
+											No results for <span class="ss__search-header__query">"{search.query.string}"</span> found.
+										</span>
+									) : (
+										<span>No results found.</span>
+									)}
+								</h1>
+							)
+						)}
 
-				{originalQuery && (
-					<div class="ss__oq">
-						Search instead for "<a href={originalQuery.url.href}>{originalQuery.string}</a>"
-					</div>
+						{originalQuery && (
+							<div class="ss__oq">
+								Search instead for "<a href={originalQuery.url.href}>{originalQuery.string}</a>"
+							</div>
+						)}
+					</>
 				)}
 			</div>
 		)
