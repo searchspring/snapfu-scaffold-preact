428a3763c454af76d175aee17ab0b7ca4c741a45
kevin <5060400+korgon@users.noreply.github.com>
2025-06-26 17:01:47 -0600

    Update recommendation.cy.js
    Update the recommendation test.

--- tests/cypress/e2e/recommendation.cy.js
+++ tests/cypress/e2e/recommendation.cy.js
@@ -10,30 +10,29 @@
  */
 
 const config = {
-	url: '', // page containing autocomplete (recommended: home/about/contact page)
+	url: 'https://localhost:3333/recommendation-default.html', // page containing recommendation
 	disableGA: '', // disable google analytic events (example: 'UA-123456-1')
+	controller: 'recommend_trending_0',
 	selectors: {
 		recommendation: {
+			target: '.ss__recs__default',
 			main: '.ss__recommendation',
 			// selector of the wrapping element. Expects child element to contain <a>
 			carousel: `.ss__recommendation .ss__carousel`,
 			result: '.ss__result',
-            resultTitle: 'a.product-thumbnail__title',
-			nextArrow: '.ss__recommendation .ss__carousel__next',
-			prevArrow: '.ss__recommendation .ss__carousel__prev',
+			resultTitle: '.ss__result__details__title',
+			nextButton: '.ss__recommendation .ss__carousel__next',
+			prevButton: '.ss__recommendation .ss__carousel__prev',
 			activeSlide: '.ss__recommendation .swiper-slide-active',
-			controller: 'recommend_similar_0',
 		},
 	},
 };
 
 describe('Recommendations', () => {
-
 	/**********************************************
-	* DO NOT MOVE, SKIP, OR EDIT THE SET UP TESTS *
-	**********************************************/
+	 * DO NOT MOVE, SKIP, OR EDIT THE SETUP TESTS *
+	 **********************************************/
 	before('Setup', () => {
-	
 		cy.wrap(config).its('url').should('have.length.at.least', 1);
 		cy.visit(config.url);
 
@@ -48,67 +47,58 @@ describe('Recommendations', () => {
 		if (config.disableGA) {
 			window[`ga-disable-${config.disableGA}`] = true;
 		}
-
 	});
 
 	describe('Tests Recommendations', () => {
-		
-		after('can click on a result and go to that page', function () {
-			cy.document().then((doc) => {
-				cy.snapController(config?.selectors?.recommendation.controller).then(({ store }) => {
-
-					cy.get(config?.selectors?.recommendation.activeSlide).should('exist');
-					let url = doc.querySelector(`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} a`).attributes?.href
-						?.value;
-					cy.get(`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} a`).eq(0)
-						.click({ force: true })
-						.then(() => {
-							cy.wait(200);
-							cy.location('href').should('include', url);
-						});
-				});
-			});
-		});
-
 		it('has a controller', function () {
-			cy.snapController(config?.selectors?.recommendation.controller).then(({ store }) => {
-				expect(store.config.globals.limits).equals(store.results.length);
-				expect(store.config.globals.product.length).to.greaterThan(0);
+			cy.snapController(config.controller).then(({ store }) => {
+				expect(store.results.length).to.greaterThan(0);
 			});
 		});
-
 
 		it('renders recommendations', function () {
-			cy.snapController(config?.selectors?.recommendation.controller).then(({ store }) => {
-				cy.get(config?.selectors?.recommendation.main).should('exist');
+			cy.snapController(config.controller).then(({ store }) => {
+				cy.get(config.selectors.recommendation.target).scrollIntoView();
 
+				cy.get(config?.selectors?.recommendation.main).should('exist');
 				cy.get(config?.selectors?.recommendation.carousel).should('exist');
 				cy.get(config?.selectors?.recommendation.result).should('exist');
 			});
 		});
 
+		it('renders products that have URL values', function () {
+			cy.snapController(config.controller).then(({ store }) => {
+				cy.get(config?.selectors?.recommendation.resultTitle)
+					.each(($title, index) => {
+						cy.wrap($title).find('a').invoke('attr', 'href').then((url) => {
+							expect(store.results[index].mappings.core.url).to.equal(url);
+						});
+					});
+			});
+		});
+
 		it('renders carousel prev buttons', function () {
 			cy.document().then((doc) => {
-				cy.snapController(config?.selectors?.recommendation.controller).then(({ store }) => {
-					cy.get(config?.selectors?.recommendation.nextArrow).should('exist');
-					cy.get(config?.selectors?.recommendation.prevArrow).should('exist');
+				cy.snapController(config.controller).then(({ store }) => {
+					cy.get(config?.selectors?.recommendation.nextButton).should('exist');
+					cy.get(config?.selectors?.recommendation.prevButton).should('exist');
 
 					cy.get(config?.selectors?.recommendation.activeSlide).should('exist');
-                    
-					//get the initial active product
+
+					// get the initial active product
 					const intialActive = doc.querySelector(
 						`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} ${config?.selectors.recommendation.resultTitle}`
 					).innerHTML;
 
-					//click the prev button
-					cy.get(config?.selectors?.recommendation.prevArrow)
-						.click({force: true})
+					// click the prev button
+					cy.get(config?.selectors?.recommendation.prevButton)
+						.click({ force: true })
 						.then(($button) => {
 							const newerActiveTitle = doc.querySelector(
 								`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} ${config?.selectors.recommendation.resultTitle}`
 							).innerHTML;
 
-							//these should not match
+							// these should not match
 							expect(newerActiveTitle).to.not.equal(intialActive);
 						});
 				});
@@ -117,32 +107,32 @@ describe('Recommendations', () => {
 
 		it('renders carousel next buttons', function () {
 			cy.document().then((doc) => {
-				cy.snapController(config?.selectors?.recommendation.controller).then(({ store }) => {
-					cy.get(config?.selectors?.recommendation.nextArrow).should('exist');
-					cy.get(config?.selectors?.recommendation.prevArrow).should('exist');
+				cy.snapController(config.controller).then(({ store }) => {
+					cy.get(config?.selectors?.recommendation.nextButton).should('exist');
+					cy.get(config?.selectors?.recommendation.prevButton).should('exist');
 
 					cy.get(config?.selectors?.recommendation.activeSlide).should('exist');
 
-					//get the initial active product
+					// get the initial active product
 					const intialActive = doc.querySelector(
 						`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} ${config?.selectors.recommendation.resultTitle}`
 					).innerHTML;
 					let newActive;
-					//click the next button
-					cy.get(config?.selectors?.recommendation.nextArrow)
-						.click({force: true})
+					// click the next button
+					cy.get(config?.selectors?.recommendation.nextButton)
+						.click({ force: true })
 						.then(($button) => {
-							//get the new active product
+							cy.wait(500);
+							// get the new active product
 							newActive = doc.querySelector(
 								`${config?.selectors?.recommendation.activeSlide} ${config?.selectors?.recommendation.result} ${config?.selectors.recommendation.resultTitle}`
-							).innerHTML;
+							).innerText;
 
-							//get the new active again
-
+							// get the new active again
 							const newerActiveIndex = doc.querySelector(`${config?.selectors?.recommendation.activeSlide}`).getAttribute('data-swiper-slide-index');
 							const storeTitle = store.results[parseInt(newerActiveIndex)].mappings.core.name;
 
-							//should have changed
+							// should have changed
 							expect(newActive).to.not.equal(intialActive);
 							expect(newActive).to.equal(storeTitle);
 						});
