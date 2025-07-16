4b74d43d4d4134e450cb6215412e534a2434dc86
searchspring-machine <machine@searchspring.com>
2025-07-16 19:29:42 +0000

    auto generated scaffolding patch from 'Patch Scaffold' action workflow

--- tests/cypress/e2e/autocomplete.cy.js
+++ tests/cypress/e2e/autocomplete.cy.js
@@ -208,3 +208,56 @@ describe('Autocomplete', () => {
 		});
 	});
 });
+
+// begin beacon tracking testing
+describe('Tracking', () => {
+	it('makes api request', function () {
+		const url = config.url;
+
+		cy.visit(url);
+		cy.addLocalSnap();
+
+		cy.waitForBundle().then(() => {
+			cy.window().then((window) => {
+				expect(window.searchspring).to.exist;
+			});
+		});
+
+		if (config.disableGA) {
+			window[`ga-disable-${config.disableGA}`] = true;
+		}
+
+		if (config.selectors.website.openInputButton) {
+			cy.get(config.selectors.website.openInputButton).first().click({ force: true });
+		}
+
+		cy.get(config.selectors.website.input).first().should('exist').focus().type(config.startingQuery, { force: true });
+
+		cy.wait('@autocomplete').should('exist');
+	});
+
+	it('tracks render', () => {
+		cy.wait(`@beacon2/autocomplete/render`).then((render) => {
+			expect(render.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+
+	// unskip the block below to add impression tracking testing
+	it.skip('tracks impression', () => {
+		cy.wait(`@beacon2/autocomplete/impression`).then((impression) => {
+			expect(impression.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+
+	it('tracks clickthrough', () => {
+		cy.get(`${config.selectors.autocomplete.result}[href], ${config.selectors.autocomplete.result} a[href]`)
+			.first()
+			.should('exist')
+			.click({ force: true });
+
+		cy.wait(`@beacon2/autocomplete/clickthrough`).then((clickthrough) => {
+			expect(clickthrough.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+});
+// end beacon tracking testing
--- tests/cypress/e2e/results.cy.js
+++ tests/cypress/e2e/results.cy.js
@@ -707,3 +707,51 @@ config?.pages?.forEach((page, _i) => {
 		});
 	});
 });
+
+// begin beacon tracking testing
+describe('Tracking', () => {
+	it('makes api request', function () {
+		const url = config.pages.find((page) => `${page.id}`.toLowerCase() == 'search')?.url || config.pages[0].url;
+
+		cy.visit(url);
+		cy.addLocalSnap();
+
+		cy.waitForBundle().then(() => {
+			cy.window().then((window) => {
+				expect(window.searchspring).to.exist;
+			});
+		});
+
+		if (config.disableGA) {
+			window[`ga-disable-${config.disableGA}`] = true;
+		}
+
+		cy.wait('@search').should('exist');
+	});
+
+	it('tracks render', () => {
+		cy.wait(`@beacon2/search/render`).then((render) => {
+			expect(render.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+
+	// unskip the block below to add impression tracking testing
+	it.skip('tracks impression', () => {
+		cy.wait(`@beacon2/search/impression`).then((impression) => {
+			expect(impression.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+
+	it('tracks clickthrough', () => {
+		cy.wait(1000); // wait for results to load
+		cy.get(`${config.selectors.results.productWrapper}[href], ${config.selectors.results.productWrapper} a[href]`)
+			.first()
+			.should('exist')
+			.click({ force: true });
+
+		cy.wait(`@beacon2/search/clickthrough`).then((clickthrough) => {
+			expect(clickthrough.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+});
+// end beacon tracking testing
