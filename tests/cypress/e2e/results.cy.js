6826c8e81175f9b82985a4418ff423840197f9ba
searchspring-machine <machine@searchspring.com>
2025-07-24 17:46:39 +0000

    auto generated scaffolding patch from 'Patch Scaffold' action workflow

--- tests/cypress/e2e/results.cy.js
+++ tests/cypress/e2e/results.cy.js
@@ -707,3 +707,44 @@ config?.pages?.forEach((page, _i) => {
 		});
 	});
 });
+
+// begin beacon tracking testing
+describe.skip('Tracking', () => {
+	it('sends beacon events', function () {
+		if (typeof config === 'undefined' || !config?.pages.length || !config.selectors.results?.productWrapper) this.skip();
+
+		const url = config?.pages?.find((page) => `${page.id}`.toLowerCase() == 'search')?.url || config.pages[0].url;
+
+		if (!url) this.skip();
+
+		cy.visit(url);
+		cy.addLocalSnap();
+
+		cy.waitForBundle().then(() => {
+			cy.window().then((window) => {
+				expect(window.searchspring).to.exist;
+			});
+		});
+
+		if (config.disableGA) {
+			window[`ga-disable-${config.disableGA}`] = true;
+		}
+
+		const firstResult = cy.get(`${config.selectors.results.productWrapper}[href], ${config.selectors.results.productWrapper} a[href]`)
+			.first()
+			.should('exist')
+			.scrollIntoView();
+
+		// impression tracking
+		cy.wait(`@beacon2/search/impression`).then((impression) => {
+			expect(impression.response.body).to.have.property('success').to.equal(true);
+		});
+
+		// click tracking
+		firstResult.trigger('click', { force: true });
+		cy.wait(`@beacon2/search/clickthrough`).then((clickthrough) => {
+			expect(clickthrough.response.body).to.have.property('success').to.equal(true);
+		});
+	});
+});
+// end beacon tracking testing
--- tests/cypress/support/e2e.js
+++ tests/cypress/support/e2e.js
@@ -51,4 +51,11 @@ beforeEach(() => {
 	cy.intercept(/.*snapui.searchspring.io\/.*.js.*$/, (req) => {
 		req.destroy();
 	});
+
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/autocomplete\/impression/, { success: true }).as('beacon2/autocomplete/impression');
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/autocomplete\/clickthrough/, { success: true }).as('beacon2/autocomplete/clickthrough');
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/search\/impression/, { success: true }).as('beacon2/search/impression');
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/search\/clickthrough/, { success: true }).as('beacon2/search/clickthrough');
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/category\/impression/, { success: true }).as('beacon2/category/impression');
+	cy.intercept('POST', /beacon.searchspring.io\/beacon\/v2\/.*\/category\/clickthrough/, { success: true }).as('beacon2/category/clickthrough');
 });
