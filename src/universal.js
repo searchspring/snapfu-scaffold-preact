dfbec6b3f006815461697e7102c12dedf7b48a29
searchspring-machine <machine@searchspring.com>
2026-01-15 22:22:58 +0000

    auto generated scaffolding patch from 'Patch Scaffold' action workflow

--- src/universal.js
+++ src/universal.js
@@ -3,16 +3,15 @@ import { polyfills } from '@searchspring/snap-preact';
 
 const promises = [];
 if (!('fetch' in window)) {
-	// @ts-ignore - types not important
 	promises.push(import('whatwg-fetch'));
 }
 if (!('Symbol' in window) || !('flatMap' in Array.prototype) || !('includes' in Array.prototype)) {
-	// @ts-ignore - types not important
 	promises.push(import('core-js/stable'));
 }
 promises.push(polyfills);
 Promise.all(promises).then(() => {
 	window.searchspring = window.searchspring || {};
+	window.searchspring.managed = true;
 	window.searchspring.build = 'universal';
 	import('./index');
 });
